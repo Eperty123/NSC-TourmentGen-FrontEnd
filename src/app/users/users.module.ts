3db759b3423d66be9c8d910719cb611143a4e54b
Niko4730 <niko4730@easv365.dk>
2021-11-29 13:21:31 +0100

    List of users
    It works

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -6,6 +6,8 @@ import { CreateComponent } from './create/create.component';
 import { DeleteComponent } from './delete/delete.component';
 import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -17,7 +19,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class UsersModule { }
