798192266cca130e5cfebee1f3c0d0e3d44f4b83
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2021-12-01 12:03:34 +0100

    get user by ID

--- src/app/users/details/details.component.ts
+++ src/app/users/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from "../shared/users.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {UserDto} from "../shared/user.dto";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-details',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private selectedId: number | undefined;
+  public selectedUser: UserDto | undefined;
+
+  detailsForm = new FormGroup( {
+    id: new FormControl({ disabled: true }),
+    username: new FormControl('', Validators.required)
+  });
+
+  constructor(private _route : ActivatedRoute, private _router : Router, private _usersService : UsersService) { }
 
   ngOnInit(): void {
+    this.selectedId = Number(this._route.snapshot.paramMap.get('id'));
+
+    this._usersService.getUser(this.selectedId).subscribe(user => {
+      this.selectedUser = user;
+      this.detailsForm.patchValue(user);
+    });
   }
 
 }
+
--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CreateComponent} from "./create/create.component";
 import {ListComponent} from "./list/list.component";
+import {DetailsComponent} from "./details/details.component";
 import {UpdateComponent} from "./update/update.component";
 
 const routes: Routes = [
   {
     path: 'update/:id',
-    component: UpdateComponent
+    component: UpdateComponent},
+
+  {
+    path: 'details/:id',
+    component: DetailsComponent
   },
   {
     path: 'create',
