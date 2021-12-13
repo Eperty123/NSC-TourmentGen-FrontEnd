cb68a28b9e0014900066a0b02ef2b536b58b6b74
Onii-chan <failer007@live.dk>
2021-12-13 14:26:16 +0100

    Added some styling to users

--- src/app/users/list/list.component.ts
+++ src/app/users/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { UserList } from '../shared/user-list';
 import { UsersService } from '../shared/users.service';
 
@@ -10,10 +11,17 @@ import { UsersService } from '../shared/users.service';
 })
 export class ListComponent implements OnInit {
   $users: Observable<UserList> | undefined;
+  $error: string | undefined;
   constructor(private _service: UsersService) { }
 
   ngOnInit(): void {
-    this.$users = this._service.getUsers();
+    this.$users = this._service.getUsers()
+      .pipe(
+        catchError(errMsg => {
+          this.$error = (errMsg ? errMsg.statusText : errMsg) as string;
+          return throwError(errMsg);
+        })
+      )
   }
 
 }
